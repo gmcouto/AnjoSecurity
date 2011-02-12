b12a99c3e15f53567e744ed2b91f6c09179f8801
Gabriel Couto <gmcouto@gmail.com>
2011-02-12 21:52:59 -0200

    Other Commit

---
+++ src/main/java/org/anjocaido/anjosecurity/App.java
@@ -0,0 +1,13 @@
+package org.anjocaido.anjosecurity;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
